93b8b1657d345a128ea85e71dc482878b1ba75a2
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2017-12-05 00:00:36 +0300

    added button

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,14 @@
 Adafruit_INA219 ina219;
 U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8;
 
+const uint8_t buttonPin = 2;     // the number of the pushbutton pin
+const uint8_t ledPin =  13;      // the number of the LED pin
+const uint16_t LONG_KEY_PRESS_INTERVAL = 1000; //1 sec is considered long keypress
+
 unsigned long previousMillis = 0;
 const unsigned int interval = 100;
-const int chipSelect = 10;
-const unsigned short StrBufSize8 = 9;
+const uint8_t chipSelect = 10;
+const uint8_t StrBufSize8 = 9;
 unsigned long currentMillis = 0;
 float shuntvoltage = 0;
 float busvoltage = 0;
@@ -24,9 +28,8 @@ float energy = 0;
 uint8_t ovf = 0;
 uint8_t cnvr = 0;
 
-// File TimeFile;
-// File VoltFile;
-// File CurFile;
+uint8_t state_btn_pressed = 0;
+uint32_t time_btn_pressed = 0;
 
 void displaydata() {
   char _float_buf[9];
@@ -82,15 +85,15 @@ void displaydata() {
 
 #ifdef DEBUG
 void debugdata() {
-  Serial.print("Timestamp:     "); Serial.print(currentMillis); Serial.println(" ms");
-  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
-  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
-  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
-  Serial.print("Power:         "); Serial.print(power); Serial.println(" mW");
-  Serial.print("Energy burned: "); Serial.print(energy); Serial.println(" mWh");
-  Serial.print("OVF:           "); Serial.println(ovf);
-  Serial.print("CNVR:          "); Serial.println(cnvr);
+  Serial.print(F("Timestamp:     ")); Serial.print(currentMillis); Serial.println(F(" ms"));
+  Serial.print(F("Bus Voltage:   ")); Serial.print(busvoltage); Serial.println(F(" V"));
+  Serial.print(F("Shunt Voltage: ")); Serial.print(shuntvoltage); Serial.println(F(" mV"));
+  Serial.print(F("Load Voltage:  ")); Serial.print(loadvoltage); Serial.println(F(" V"));
+  Serial.print(F("Current:       ")); Serial.print(current_mA); Serial.println(F(" mA"));
+  Serial.print(F("Power:         ")); Serial.print(power); Serial.println(F(" mW"));
+  Serial.print(F("Energy burned: ")); Serial.print(energy); Serial.println(F(" mWh"));
+  Serial.print(F("OVF:           ")); Serial.println(ovf);
+  Serial.print(F("CNVR:          ")); Serial.println(cnvr);
   Serial.println("");
 }
 #endif
@@ -112,6 +115,12 @@ void setup() {
   Serial.begin(115200);
   Serial.println(F("Start debugging output ..."));
   #endif
+
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+
   // SD.begin(chipSelect);
   u8x8.begin();
   ina219.begin();
@@ -119,30 +128,53 @@ void setup() {
 
 void loop() {
   currentMillis = millis();
+
+  // read the state of the pushbutton value:
+  uint8_t buttonState = digitalRead(buttonPin);
+
+  if (state_btn_pressed) { //key was being pressed in the last cycle
+    if (buttonState == HIGH) {
+      // the user keeps pressing the button
+      // do smth special
+    } else {
+      // we have the key was pressed down and just released
+      state_btn_pressed = 0;
+      uint16_t btn_pressed_interval = currentMillis - time_btn_pressed;
+      // time_btn_released = currentMillis;
+      if (btn_pressed_interval < LONG_KEY_PRESS_INTERVAL) {
+        // it was short key press
+        #ifdef DEBUG
+        Serial.println(F("BUTTON HAS BEEN SHORT PRESSED"));
+        #endif
+      } else {
+        // it was long key press
+        #ifdef DEBUG
+        Serial.println(F("BUTTON HAS BEEN LONG PRESSED"));
+        #endif
+      }
+    }
+  } else { // no keys was pressed in the last cycle
+    if (buttonState == HIGH) {
+      state_btn_pressed = 1;
+      time_btn_pressed = currentMillis;
+    }
+  }
+
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     ina219values();
     displaydata();
     #ifdef DEBUG
     debugdata();
-    delay(0);
+    delay(200);
     #endif
-    // TimeFile = SD.open("TIME.txt", FILE_WRITE);
-    // if (TimeFile) {
-    //   TimeFile.println(currentMillis);
-    //   TimeFile.close();
-    // }
-    //
-    // VoltFile = SD.open("VOLT.txt", FILE_WRITE);
-    // if (VoltFile) {
-    //   VoltFile.println(loadvoltage);
-    //   VoltFile.close();
-    // }
-    //
-    // CurFile = SD.open("CUR.txt", FILE_WRITE);
-    // if (CurFile) {
-    //   CurFile.println(current_mA);
-    //   CurFile.close();
-    // }
   }
 }
