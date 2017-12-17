81784ea9a363338805dfb510d8b7893bfbef7b2a
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2017-12-17 22:18:30 +0300

    finished power_meter mode

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ void read_ina219() {
   busvoltage = ina219.getBusVoltage_V();
   current_mA = ina219.getCurrent_mA();
   power = ina219.getPower_mW();
-  energy += power / 3600;  //TODO wtf
+  energy += power * (timeNow - timePrevious) / 3600000;
   ovf = ina219.getOVF();
   cnvr = ina219.getCNVR();
 }
@@ -68,15 +68,15 @@ void clear_display() {
 }
 
 void displaydata_VA_METER() {
-  char _float_buf[9];
   u8x8.setFont(u8x8_font_artossans8_r);
   // u8x8.setFont(u8x8_font_pxplustandynewtv_f);
 
   // Voltmeter
   u8x8.setCursor(0, 0);
   u8x8.print(F("V:"));
-  dtostrf(busvoltage, 7, ina219.getVbusDigitsAfterPoint(), _float_buf);  /* 8 is mininum width, 2 is precision */
-  u8x8.draw2x2String(2, 0, _float_buf);
+  char _float_buf1[9];
+  dtostrf(busvoltage, 7, ina219.getVbusDigitsAfterPoint(), _float_buf1);  /* 8 is mininum width, 2 is precision */
+  u8x8.draw2x2String(2, 0, _float_buf1);
 
   // Ampermeter
   u8x8.setCursor(0, 2);
@@ -84,8 +84,8 @@ void displaydata_VA_METER() {
   if (ovf) {
     u8x8.setInverseFont(1);
   }
-  dtostrf(current_mA, 8, ina219.getCurrentDigitsAfterPoint(), _float_buf);  /* 8 is mininum width, 2 is precision */
-  u8x8.draw2x2String(0, 3, _float_buf);
+  dtostrf(current_mA, 8, ina219.getCurrentDigitsAfterPoint(), _float_buf1);  /* 8 is mininum width, 2 is precision */
+  u8x8.draw2x2String(0, 3, _float_buf1);
   u8x8.setInverseFont(0);
 
   // Scale info
@@ -98,31 +98,92 @@ void displaydata_VA_METER() {
   u8x8.setCursor(0, 7);
   u8x8.print(F(" "));
   u8x8.print(_float_buf2);
-  u8x8.print(F("A   "));
+  u8x8.print(F("A  "));
   u8x8.print(_float_buf3);
   u8x8.print(F("mA"));
 }
 
 void displaydata_POWER_METER() {
-  char _float_buf[9];
   // u8x8.setFont(u8x8_font_pressstart2p_f);
   u8x8.setFont(u8x8_font_artossans8_r);
-  // lines 0-1 Voltmeter
+  // Voltmeter
   u8x8.setCursor(0, 0);
-  u8x8.print(F("V:"));
-  dtostrf(busvoltage, 7, ina219.getVbusDigitsAfterPoint(), _float_buf);  /* 8 is mininum width, 2 is precision */
-  u8x8.drawString(2, 0, _float_buf);
+  u8x8.print(F("Vbus:"));
+  char _float_buf8[9];
+  dtostrf(busvoltage, 8, 3, _float_buf8);
+  u8x8.print(_float_buf8);
+  u8x8.print(F(" V"));
 
-  // lines 2-4 Ampermeter
-  u8x8.setCursor(0, 2);
-  u8x8.print(F("mA ("));
-  u8x8.print(ina219.getScale());
-  u8x8.print(F("):"));
+  u8x8.setCursor(0, 1);
+  u8x8.print(F("Vsh:"));
+  dtostrf(shuntvoltage, 8, 2, _float_buf8);
+  u8x8.print(_float_buf8);
+  u8x8.print(F(" mV"));
+
+  // Ampermeter
+  u8x8.setCursor(0, 3);
+  u8x8.print(F("Ish:"));
   if (ovf) {
     u8x8.setInverseFont(1);
   }
-  dtostrf(current_mA, 8, ina219.getCurrentDigitsAfterPoint(), _float_buf);  /* 8 is mininum width, 2 is precision */
-  u8x8.drawString(0, 3, _float_buf);
+  dtostrf(current_mA, 8, ina219.getCurrentDigitsAfterPoint(), _float_buf8);
+  u8x8.print(_float_buf8);
+  u8x8.print(F(" mA"));
+  u8x8.setInverseFont(0);
+
+  // Scale info
+  u8x8.setCursor(0, 4);
+  u8x8.print(F("("));
+  char _float_buf5[6];
+  dtostrf(ina219.getCurrentOverflow(), 5, 3, _float_buf5);
+  char _float_buf4[5];
+  dtostrf(ina219.getCurrentLSB(), 4, 2, _float_buf4);
+  u8x8.print(_float_buf5);
+  u8x8.print(F("A, "));
+  u8x8.print(_float_buf4);
+  u8x8.print(F("mA)"));
+
+  // power
+  u8x8.setCursor(0, 6);
+  u8x8.print(F("Pwr:"));
+  dtostrf(power, 8, 1, _float_buf8);
+  u8x8.print(_float_buf8);
+  u8x8.print(F(" mW"));
+
+  // energy
+  u8x8.setCursor(0, 7);
+  u8x8.print(F("Enr:"));
+  dtostrf(energy, 8, 1, _float_buf8);
+  u8x8.print(_float_buf8);
+  u8x8.print(F(" mWh"));
+}
+
+void displaydata_RAW_MODE() {
+  // u8x8.setFont(u8x8_font_pressstart2p_f);
+  u8x8.setFont(u8x8_font_artossans8_r);
+  // Voltmeter
+  u8x8.setCursor(0, 0);
+  u8x8.print(F("Vbus:"));
+  char _float_buf8[9];
+  dtostrf(busvoltage, 8, 3, _float_buf8);
+  u8x8.print(_float_buf8);
+  u8x8.print(F(" V"));
+
+  u8x8.setCursor(0, 1);
+  u8x8.print(F("Vsh:"));
+  dtostrf(shuntvoltage, 8, 2, _float_buf8);
+  u8x8.print(_float_buf8);
+  u8x8.print(F(" mV"));
+
+  // lines 2-4 Ampermeter
+  u8x8.setCursor(0, 2);
+  u8x8.print(F("Ish:"));
+  if (ovf) {
+    u8x8.setInverseFont(1);
+  }
+  dtostrf(current_mA, 8, ina219.getCurrentDigitsAfterPoint(), _float_buf8);
+  u8x8.print(_float_buf8);
+  u8x8.print(F(" mA"));
   u8x8.setInverseFont(0);
 
   // line 5
@@ -182,9 +243,9 @@ void loop() {
         mode = TO_POWER_METER;
       }
       if (timeNow - timePrevious >= interval) {
-        timePrevious = timeNow;
         read_ina219();
         displaydata_VA_METER();
+        timePrevious = timeNow;
       }
       break;
 
@@ -204,9 +265,9 @@ void loop() {
         mode = TO_VA_METER;
       }
       if (timeNow - timePrevious >= interval) {
-        timePrevious = timeNow;
         read_ina219();
         displaydata_POWER_METER();
+        timePrevious = timeNow;
       }
       break;
 
